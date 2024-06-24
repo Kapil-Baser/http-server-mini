bff5036b184f945b7440eac9c29fdc3967535479
Kapil-Baser <kapil.kumar.baser@gmail.com>
2024-06-24 08:51:01 +0530

    pass stage

--- app/server.c
+++ app/server.c
@@ -125,15 +125,16 @@ void *process_request(void *socket_fd)
 			char *file_name = url;
 			printf("File name %s\n", file_name);
 			char file_path[BUFF_SIZE];
-			//snprintf(file_path, sizeof(file_path), "%s%s.txt", directory, file_name);
-			//printf("File path - %s\n", file_path);
+			snprintf(file_path, sizeof(file_path), "%s.txt", file_name);
+			printf("File path - %s\n", file_path);
 			char *cont_len = strstr(buf, "Content-Length:");
 			cont_len = cont_len + 16;
 			printf("Cont len - %s", cont_len);
-			char *content_len = strstr(buf, "Content-Type:");
-			char *token = strtok(content_len, "\r\n");
+			char *content_type = strstr(buf, "Content-Type:");
+			char *token = strtok(content_type, "\r\n");
 			token = strtok(NULL, "\r\n");
 			printf("Contents - %s\n", token);
+			
 			// writing the contents into file
 			//FILE *fp = fopen(file_path, "w");
 			//fwrite(token, 1, sizeof(token) - 1, fp);
